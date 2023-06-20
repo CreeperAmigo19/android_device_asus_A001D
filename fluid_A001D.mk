c42457b84a7df84b53ab6be4352d83d1333c360b
criperm19 <96837536+criperm19@users.noreply.github.com>
2023-06-20 09:05:35 -0300

    Update and rename lmodroid_A001D.mk to fluid_A001D.mk

--- fluid_A001D.mk
+++ fluid_A001D.mk
@@ -22,11 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/A001D/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lmodroid/config/common.mk)
+$(call inherit-product, vendor/fluid/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := A001D
-PRODUCT_NAME := lmodroid_A001D
+PRODUCT_NAME := fluid_A001D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_A001D
 PRODUCT_MANUFACTURER := asus
